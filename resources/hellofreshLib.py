2f5130d9ac2a55c6f07484419b95b81f65f9d678
Bypus <joris.gre@hotmail.fr>
2024-10-13 02:38:25 +0200

    recettes francaises avec images et limite d'ingrédients

--- resources/hellofreshLib.py
+++ resources/hellofreshLib.py
@@ -24,32 +24,29 @@ def search_image(query):
     else:
         return None
 
-# Load the JSON file
-with open('resources/translated_recipes.json', 'r', encoding='utf-8') as file:
-    data = json.load(file)
-
 # Fonction pour retirer les accents d'une chaîne
 def remove_accents(input_str):
     input_str = input_str.replace('œ', 'oe').replace('æ', 'ae')
     nfkd_form = unicodedata.normalize('NFKD', input_str)
     return "".join([c for c in nfkd_form if not unicodedata.combining(c)])
 
-def get_recipe_embed(ingredients: str):
+# Load the JSON file
+with open('resources/recipes.json', 'r', encoding='utf-8') as file:
+    data = json.load(file)
+
+def get_recipe_embed(ingredients: str, facile: bool = True):
     # Split the input ingredients string into individual ingredients
     ingredient_list = ingredients.split()
     normalized_ingredient_list = [remove_accents(ingredient.lower()) for ingredient in ingredient_list]
-
+    
     # Find matching recipes where all ingredients are in the recipe name
     matching_recipes = []
     for recipe in data:  # Loop over dictionary items
         # Check if all ingredients are found in the recipe name
-        # print(recipe['name'])
         normalized_recipe_name = remove_accents(recipe['name'].lower())
 
         if all(ingredient in normalized_recipe_name for ingredient in normalized_ingredient_list):
             matching_recipes.append(recipe)
-            words = normalized_recipe_name.split()
-            first_three_words = ' '.join(words[:3])
 
     # If no matching recipes are found
     if not matching_recipes:
@@ -58,20 +55,26 @@ def get_recipe_embed(ingredients: str):
             description=f"No recipes found with ingredients: {', '.join(ingredient_list)}",
             color=discord.Color.red()
         )
-    
+
+    # If 'facile' is set to 1, find recipes with the minimum number of ingredients
+    if facile:
+        # Find the minimum number of ingredients among the matching recipes
+        min_ingredients = min(len(recipe['ingredients']) for recipe in matching_recipes)
+        # Filter the recipes to get those with the minimum number of ingredients
+        matching_recipes = [recipe for recipe in matching_recipes if len(recipe['ingredients']) == min_ingredients]
+
     # Select a random matching recipe
     selected_recipe = random.choice(matching_recipes)
-
-    image_url = search_image(first_three_words)
 
     # Create the embed message
     embed_name = discord.Embed(
         title=selected_recipe['name'],
+        url=selected_recipe['url'],
         description=selected_recipe['description'],
         color=discord.Color.green()
     )
-    embed_name.set_image(url=image_url)
-    embed_name.set_footer(text="Immage non contractuelle")
+    embed_name.set_image(url=selected_recipe['image_url'])
+    embed_name.set_footer(text="Image non contractuelle")
 
     recipe_ingredients = ""
     for ingredient in selected_recipe['ingredients']:
@@ -82,7 +85,7 @@ def get_recipe_embed(ingredients: str):
         title="Ingrédients et instructions",
         color=discord.Color.green()
     )
-    embed_cook.add_field(name="Ingredients", value=recipe_ingredients, inline=False)
+    embed_cook.add_field(name="Ingredients pour 1 personne", value=recipe_ingredients, inline=False)
     step_number = 0
     for step in selected_recipe['instructions']:
         step_number += 1
